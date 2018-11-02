ad7f31e5fc5032df6de3f4078a4e53f9893f2570
tomus151 <ciasteczkowy15@gmail.com>
2018-11-02 19:07:22 +0100

    utworzony i podpięty komponenyt categories

---
+++ src/components/Categories.vue
@@ -0,0 +1,19 @@
+<template lang="html">
+  <div class="categories-wrapper">
+      categories
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Categories"
+}
+</script>
+
+<style lang="scss">
+    .categories-wrapper{
+        width:100%;
+        height:100%;
+        background:yellow;
+    }
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import Categories from '@/components/Categories'
 import Fourofour from '@/components/Fourofour'
 
 Vue.use(Router)
@@ -12,6 +13,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+        path: '/categories',
+        name: 'Categories',
+        component: Categories
     },
     {
         path:'*',
