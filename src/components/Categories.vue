8e37171fee485c8687a4da66e318c28e5f1b3097
tomus151 <ciasteczkowy15@gmail.com>
2018-11-02 22:43:20 +0100

    dodana lista produktów

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -1,29 +1,106 @@
 <template lang="html">
   <div class="categories-wrapper">
-      <div class="product-box-1">
-
-      </div>
-      <div class="product-box-2">
-
-      </div>
-      <div class="product-box-3">
-
-      </div>
-      <div class="product-box-4">
-
-      </div>
-      <div class="product-box-5">
-
-      </div>
-      <div class="product-box-6">
-
-      </div>
+        <div class="product-row">
+            <ul>
+                <li v-for="(value, index) in productsFirst">
+                    <div class="product-box">
+                        <div class="product-image-container">
+                            <img :src="value.imageUrl" alt="" class="product-image">
+                        </div>
+                        <div class="product-name-container">
+                            <span class="product-name">
+                                {{value.name}}
+                            </span>
+                        </div>
+                        <div class="product-description-container">
+                            <span class="product-description">
+                                {{value.description}}
+                            </span>
+                        </div>
+                        <div class="product-price-box">
+                            <span class="product-price">
+                                {{value.prece}}
+                            </span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+        <div class="product-row">
+            <ul>
+                <li v-for="(value, index) in productsSecounds">
+                    <div class="product-box">
+                        <div class="product-image-container">
+                            <img :src="value.imageUrl" alt="" class="product-image">
+                        </div>
+                        <div class="product-name-container">
+                            <span class="product-name">
+                                {{value.name}}
+                            </span>
+                        </div>
+                        <div class="product-description-container">
+                            <span class="product-description">
+                                {{value.description}}
+                            </span>
+                        </div>
+                        <div class="product-price-container">
+                            <span class="product-price">
+                                {{value.prece}}
+                            </span>
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
   </div>
 </template>
 
 <script>
 export default {
-    name:"Categories"
+    name:"Categories",
+    data(){
+        return{
+            productsFirst:[
+                {
+                    nr:"1",
+                    imageUrl:"@/assets/logo.png",
+                    name:"RED SEAT",
+                    description:"Lorem ipsum dolor sit amet",
+                    price:"$45"
+                },
+                {
+                    nr:"2",
+                    imageUrl:"",
+                    name:"WHITE TABLE",
+                    description:"Lorem ipsum dolor sit amet",
+                    price:"$350"
+                },
+            ],
+            productsSecounds:[
+                {
+                    nr:"3",
+                    name:"BLUE SEAT",
+                    description:"Lorem ipsum dolor sit amet",
+                    price:"$35",
+                    imageUrl:""
+                },
+                {
+                    nr:"4",
+                    imageUrl:"",
+                    name:"MODERN BEAD",
+                    description:"Lorem ipsum dolor sit amet",
+                    price:"$120"
+                },
+                {
+                    nr:"5",
+                    imageUrl:"aaaa",
+                    name:"DARK SEAT",
+                    description:"Lorem ipsum dolor sit amet",
+                    price:"$35"
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -34,6 +111,27 @@ export default {
         background:#fff;
         display:flex;
         flex-direction:column;
+        align-items: center;
+        justify-content: center;
+        .product-row{
+            width:50%;
+            background:yellow;
+            margin-bottom:20px;
+            display:flex;
+            .product-box{
+                width:25%;
+                display:flex;
+                justify-content: center;
+                border:1px solid black;
+                margin:5px;
+                &.product-box-4{
+                    width: 50%;
+                }
+                &.product-box-2{
+                    width:75%;
+                }
+            }
+        }
 
     }
 </style>
