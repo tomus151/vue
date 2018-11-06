f60f3408e752513fe495c15cee7edb5562380fe7
tomus151 <ciasteczkowy15@gmail.com>
2018-11-06 15:24:43 +0100

    ostaylowanie listy produktow

--- src/components/Categories.vue
+++ src/components/Categories.vue
@@ -7,20 +7,22 @@
                         <div class="product-image-container">
                             <img :src="value.imageUrl" alt="" class="product-image">
                         </div>
-                        <div class="product-name-container">
-                            <span class="product-name">
-                                {{value.name}}
-                            </span>
-                        </div>
-                        <div class="product-description-container">
-                            <span class="product-description">
-                                {{value.description}}
-                            </span>
-                        </div>
-                        <div class="product-price-box">
-                            <span class="product-price">
-                                {{value.price}}
-                            </span>
+                        <div class="descriptions-container">                       
+                            <div class="product-name-container">
+                                <span class="product-name">
+                                    {{value.name}}
+                                </span>
+                            </div>
+                            <div class="product-description-container">
+                                <span class="product-description">
+                                    {{value.description}}
+                                </span>
+                            </div>
+                            <div class="product-price-box">
+                                <span class="product-price">
+                                    {{value.price}}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 </li>
@@ -104,64 +106,48 @@ export default {
                     padding:20px;
                     background:#fff;
                     max-height:300px;
-                &:nth-child(1){
-                    width:calc( 25% - 20px);
-                }
-                &:nth-child(2){
-                    width:calc( 75% - 20px);
-                    .product-box{
-                        display:flex;
-                        align-items: center;
-                        height:100%;
-                        .product-image-container{
-                            max-width:50%;
-                        }
+                    &:nth-child(1){
+                        width:calc( 25% - 20px);
                     }
-                }
-                &:nth-child(3){
-                    width:calc( 25% - 20px);
-                }
-                &:nth-child(4){
-                    width:calc( 50% - 20px);
-                    .product-box{
-                        height:100%;
-                        display:flex;
-                        align-items: center;
-                        .product-image-container{
-                            max-width:50%;
-                        }
-                    }
-                }
-                &:nth-child(5){
-                    width:calc( 25% - 20px);
-                }
-                list-style: none;
-                .product-box{
-                    .product-image-container{
-                        max-height:100%;
-                        img{
-                            width:100%;
+                    &:nth-child(2){
+                        width:calc( 75% - 20px);
+                        .product-box{
+                            display:flex;
+                            align-items: center;
                             height:100%;
+                            .product-image-container{
+                                max-width:50%;
+                            }
                         }
                     }
-                }
-                    // width:33.33%;
-                    // .product-box{
-                    //     width:25%;
-                    //     display:flex;
-                    //     justify-content: center;
-                    //     border:1px solid black;
-                    //
-                    //     &.product-box-3{
-                    //         width: 50%;
-                    //     }
-                    //     &.product-box-1{
-                    //         width:75%;
-                    //     }
-                    //     img{
-                    //         width:100%;
-                    //     }
-                    // }
+                    &:nth-child(3){
+                        width:calc( 25% - 20px);
+                    }
+                    &:nth-child(4){
+                        width:calc( 50% - 20px);
+                        .product-box{
+                            height:100%;
+                            display:flex;
+                            align-items: center;
+                            .product-image-container{
+                                max-width:50%;
+                            }
+                        }
+                    }
+                    &:nth-child(5){
+                        width:calc( 25% - 20px);
+                    }
+                    list-style: none;
+                    .product-box{
+                        .product-image-container{
+                            max-height:100%;
+                            height:100%;
+                            img{
+                                width:100%;
+                                height:100%;
+                            }
+                        }
+                    }
                 }
             }
         }
